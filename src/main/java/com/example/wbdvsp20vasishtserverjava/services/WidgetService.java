b913c5a24f26332c769609521af83c2e89454e8c
vasisht54 <vasisht.theviper@gmail.com>
2020-04-09 13:49:10 -0400

    Service bug fix

--- src/main/java/com/example/wbdvsp20vasishtserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp20vasishtserverjava/services/WidgetService.java
@@ -10,8 +10,8 @@ public class WidgetService {
 
     {
         widgetList.add(new Widget("1","Heading Widget A", "5e867262d9496e00174f5e7c", "HEADING", 1, "Sample Heading text",null,1,0,0,null,null,null));
-        widgetList.add(new Widget("2", "Heading Widget B", "5e867262d9496e00174f5e7c","HEADING",2, "Sample Heading text",null,1,0,0,null,null,null));
-        widgetList.add(new Widget("3","Heading Widget C", "5e867262d9496e00174f5e7c", "HEADING",3, "Sample Heading text",null,1,0,0,null,null,null));
+        widgetList.add(new Widget("2", "Heading Widget B", "5e867262d9496e00174f5e7c","HEADING",1, "Sample Heading text",null,3,0,0,null,null,null));
+        widgetList.add(new Widget("3","Paragraph Widget C", "5e867262d9496e00174f5e7c", "PARAGRAPH",1, "Sample Paragraph text",null,1,0,0,null,null,null));
 
         widgetList.add(new Widget("4","Heading Widget Z", "5e867261d9496e00174f5e7b", "HEADING",1, "Sample Heading text",null,1,0,0,null,null,null));
         widgetList.add(new Widget("5","Paragraph Widget X", "5e867261d9496e00174f5e7b", "PARAGRAPH",2, "Sample Paragraph text",null,1,0,0,null,null,null));
